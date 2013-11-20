b4b4f5cd6800f2aa1cbc2abd44a48f6e9cdef645
baconstarvation <ant.ucd@gmail.com>
2013-11-19 23:40:59 -0800

    new relic again

---
+++ app/newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name : ['Bloksy'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key : '7ef4b6cb3631a752be33341ec0beb60cc2133d6b',
+  logging : {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level : 'trace'
+  }
+};
